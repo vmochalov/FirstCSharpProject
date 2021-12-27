9d0cbb6a6abf1514b9d5f4fc52d0ea9d2c279396
Sefi Mochalov <mochalov.vsevolod@gmail.com>
2021-12-27 16:32:58 +0200

    Chapter 9

--- FirstCSharpProject/Program.cs
+++ FirstCSharpProject/Program.cs
@@ -10,9 +10,12 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            byte x = 10;
-            Console.WriteLine(x + "\r\n" + 11);
-            Console.WriteLine("HeyHey");
+            //byte x = 10; //chapter 8
+            //Console.WriteLine(x + "\r\n" + 11); //chapter 8
+            //Console.WriteLine("HeyHey"); //chapter 8
+            double x = 9.99;
+            int y = (int)x;
+            Console.WriteLine(y);
             Console.ReadKey();
         }
     }
