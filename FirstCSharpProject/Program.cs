32c3c23c7da0bc8110902ebbc1ffbe031d26dcea
Sefi Mochalov <mochalov.vsevolod@gmail.com>
2021-12-27 10:20:13 +0200

    first commit

---
+++ FirstCSharpProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yuda the King!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("QA in Centrical are the best!");
+            Console.ReadKey();
+        }
+    }
+}
