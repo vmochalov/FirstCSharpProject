20f860cb424a9141471846b6d407dfda7b87c752
Sefi Mochalov <mochalov.vsevolod@gmail.com>
2021-12-29 09:23:42 +0200

    13 - Reference data types - Array

--- FirstCSharpProject/Program.cs
+++ FirstCSharpProject/Program.cs
@@ -16,6 +16,43 @@ namespace MyFirstProject
             /* double x = 9.99;
             int y = (int)x;
             Console.WriteLine(y); */ //chapter 9
+
+            // Chapter 12 - Array
+
+            /* int[] numbers = { 1, 2, 3, 4, 5, 6, };
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers.Length); */
+
+            /* string[] beatles = { "John", "Paul", "George", "Ringo" };
+            string[] queen = { "Freddie", "Brian", "Roger", "John" };
+            bool result = beatles.Equals(queen);
+            Console.WriteLine(result); */
+
+            /* string s1 = "29/12/2021";
+            string[] result = s1.Split('/');
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+            Console.WriteLine(result[2]); */
+
+            /* string date = "21/12/2021";
+            string[] result = date.Split('1');
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+            Console.WriteLine(result[2]); */
+
+            /* string[,] names = new string[2, 3];
+            names[0, 0] = "Sefi";
+            names[0, 1] = "Alex";
+            names[0, 2] = "Yonatan";
+            names[1, 0] = "Sharon";
+            names[1, 1] = "Jackie";
+            names[1, 2] = "Anatoli";
+            Console.WriteLine(names[0, 0]); */
+
+            /* string[,] names1 = { { "Jackie", "Alex", "Sharon" }, { "Anatoli", "Yuda", "Adiel" } };
+            Console.WriteLine(names1[1, 2]); */
+
+
             Console.ReadKey(); 
         }
     }
